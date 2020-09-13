1f518466897bd60463b07f67cd5d5d4b75d4bc8e
Yevhen Malyshev <meugen.82@gmail.com>
2020-09-13 21:45:32 +0300

    Started modifying project to composable

--- app/src/main/java/meugeninua/adaptermemoryleak/MainActivity.kt
+++ app/src/main/java/meugeninua/adaptermemoryleak/MainActivity.kt
@@ -2,10 +2,26 @@ package meugeninua.adaptermemoryleak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContent { greeting(name = "meugen") }
+    }
+
+    @Composable
+    fun greeting(name: String) {
+        Text(text = "Hello, $name!")
+    }
+
+    @Preview
+    @Composable
+    fun previewGreeting() {
+        greeting(name = "meugen")
     }
 }
